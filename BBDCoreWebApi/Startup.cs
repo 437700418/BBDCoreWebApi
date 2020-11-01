d6d6f209bfceb93116a4bc9aa8531c3097d1ba2a
437700418 <437700418@qq.com>
2020-11-01 16:27:23 +0800

    去掉IIS依赖

--- BBDCoreWebApi/Startup.cs
+++ BBDCoreWebApi/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,7 @@ namespace BBDCoreWebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+         
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,6 +48,7 @@ namespace BBDCoreWebApi
             {
                 endpoints.MapControllers();
             });
+           
         }
     }
 }
