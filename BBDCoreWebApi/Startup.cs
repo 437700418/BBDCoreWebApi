185107075231d71985f5a09d8d33d52f8ce50aaf
437700418@qq.com <LAOZIbaobaodong1119>
2020-11-22 09:38:50 +0800

    去掉多余代码

--- BBDCoreWebApi/Startup.cs
+++ BBDCoreWebApi/Startup.cs
@@ -76,15 +76,6 @@ namespace BBDCoreWebApi
             }
 
             app.UseHttpsRedirection();
-            //app.UseSwagger();
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint($"/swagger/V1/swagger.json", $"{ApiName} V1");
-
-            //    //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件,
-            //    //注意localhost:8001/swagger是访问不到的，去launchSettings.json把launchUrl去掉，如果你想换一个路径，直接写名字即可，比如直接写c.RoutePrefix = "doc";
-            //    c.RoutePrefix = "";
-            //});
 
             app.UseSwaggerMildd();
             app.UseRouting();
